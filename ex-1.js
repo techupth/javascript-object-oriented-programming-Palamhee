f0ba78dd026e91144e38d2ebceae2f0eb66a1af1
Palamhee <palarat.pj@gmail.com>
2024-04-28 02:56:44 +1200

    hand in OOP assignment

--- ex-1.js
+++ ex-1.js
@@ -1,3 +1,27 @@
-class EmailNotification { }
+class EmailNotification {
+    constructor(notificationID,createdTime,content,receiver) {
+        this.notificationID = notificationID;
+        this.createdTime = createdTime;
+        this.content = content;
+        this.receiver = receiver;
+    }
+    send() {
+        console.log(`Notification has been sent to ${this.receiver} `)
+    }
+}
 
-class SMSNotification { }
+class SMSNotification {
+    constructor(notificationID,createdTime,content,phoneNumber) {
+        this.notificationID = notificationID;
+        this.createdTime = createdTime;
+        this.content = content;
+        this.phoneNumber = phoneNumber;
+    }
+    send() {
+        console.log(`Notification has been sent to ${this.phoneNumber}`)
+    }
+}
+const palamheeEmail = new EmailNotification("palamhee1234",19.19,"Hello, world!","palarat.pj@gmail.com");
+const palamheeSMS = new SMSNotification("palamhee1234",19.19,"Hello, world!","0291297504");
+palamheeEmail.send();
+palamheeSMS.send();
--- ex-2.js
+++ ex-2.js
@@ -1,5 +1,31 @@
-class Notification { }
+class Notification {
+    constructor(notificationId,createdTime,content,receiver) {
+        this.notificationId = notificationId;
+        this.createdTime = createdTime;
+        this.content = content;
+        this.receiver = receiver;
+    }
+    send() {
+        console.log(`Notification has been sent to ${this.receiver}`)
+    }
+}
 
-class EmailNotification { }
+class EmailNotification extends Notification {
+    constructor(notificationId,createdTime,content,receiver) {
+        super(notificationId,createdTime,content,receiver);
+    }
+}
 
-class SMSNotification { }
+class SMSNotification extends Notification {
+    constructor(notificationId,createdTime,content,phoneNumber) {
+        super(notificationId,createdTime,content)
+        this.phoneNumber = phoneNumber;
+    }
+    send() {
+        console.log(`Notification has been sent to ${this.phoneNumber}`)
+    }
+}
+const palamheeEmail = new EmailNotification("palamhee1234",19.19,"Hello, world!","palarat.pj@gmail.com");
+const palamheeSMS = new SMSNotification("palamhee1234",19.19,"Hello, world!","0291297504");
+palamheeEmail.send();
+palamheeSMS.send();
